61383a9321c7323a7d2c90aed928b127bb11d3ab
lucapette <ciao@lucapette.me>
2022-12-17 14:41:39 +0100

    Ship theme toggle

--- main/assets/main.js
+++ main/assets/main.js
@@ -1,4 +1,12 @@
-import { createIcons, Mail, Twitter, Linkedin, Github } from "lucide";
+import {
+  createIcons,
+  Mail,
+  Twitter,
+  Linkedin,
+  Github,
+  Moon,
+  Sun,
+} from "lucide";
 
 createIcons({
   icons: {
@@ -6,8 +14,20 @@ createIcons({
     Twitter,
     Linkedin,
     Github,
+    Moon,
+    Sun,
   },
 });
+
+function toggleTheme() {
+  document.documentElement.classList.toggle("dark");
+  localStorage.setItem(
+    "theme",
+    localStorage.getItem("theme") == "light" ? "dark" : "light"
+  );
+}
+
+window.toggleTheme = toggleTheme;
 
 console.info(
   "Hi 👋👋👋. Grab the code here: https://github.com/lucapette/website"
